c25782978c7be9ddbc18c9e0b2cc59a9fb5188e5
dkoeplin <dkoeplinger@gmail.com>
2016-09-23 17:32:18 -0700

    adding parse-log.py

---
+++ static/parse-log.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os, sys, datetime
+
+def pad(i):
+  s = str(i)
+  while len(s) < 4:
+    s = "0" + s
+  return s
+
+def main():
+  logDir = os.getcwd() + "/logs/"
+  if not os.path.exists(logDir):
+    os.makedirs(logDir)
+  else:
+    os.system("rm " + logDir + "/*.log")
+
+  traversal = 1
+  out = open(logDir + pad(0) + "_Heading.log", "w")
+
+  with open("compile.log", "r") as f:
+    lines = f.readlines()
+    i = 0
+    while i < len(lines):
+      line = lines[i]
+      if "Starting traversal" in line:
+        out.close()
+        out = open(logDir + pad(traversal) + "_" + line[19:] + ".log", "w")
+        traversal += 1
+      elif line.startswith("Exception"):
+        out.close()
+        out = open(logDir + pad(traversal) + "_Exception.log", "w")
+        traversal += 1
+
+      out.write(line)
+      i += 1
+
+  out.close()
+
+if __name__ == "__main__":
+    main()
