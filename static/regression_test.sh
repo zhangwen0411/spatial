4a73983470880d52e8f0afc7753dcce716f1cb75
Matthew Feldman <mattfel@stanford.edu>
2016-10-23 11:42:43 -0700

    Pagerank fix

--- static/regression_test.sh
+++ static/regression_test.sh
@@ -16,7 +16,7 @@ dense_test_list=("DotProduct" "MatMult_inner" "TPCHQ6" "BlackScholes" "MatMult_o
 dense_args_list=("9600"       "8 192 192"     "1920"   "960"          "8 192 192"    
 	"96 8 96" "8 192 192" "96 96"  "96 96" "768 2"     "192 192")
 sparse_test_list=("BFS" "PageRank" "TriangleCounting" "SparseSGD" "TPCHQ1")
-sparse_args_list=("960" "960"      "960"              "960"       "960"   )    
+sparse_args_list=("960" "2 384 1"      "960"              "960"       "960"   )    
 
 # Seconds to pause while waiting for apps to run
 delay=1200
