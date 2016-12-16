cc26bf9298c2d9cfbc8ab25e3efd62406311de35
Matthew Feldman <mattfel@stanford.edu>
2016-12-15 19:04:48 -0800

    extend reg test delay and kinda fix devicememcpy

--- scripts/regression_functions.sh
+++ scripts/regression_functions.sh
@@ -4,8 +4,8 @@
 ##   It is called from the receive.sh, which handles path variables
 ##   and git checkouts on a server-specific basis
 
-spacing=40
-delay=2000
+spacing=45
+delay=2100
 numpieces=30
 hist=72
 
